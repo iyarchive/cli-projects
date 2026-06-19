ff766e29f080e3e0d76881abba38b581769f93ca
iya <asiiyaaahh@gmail.com>
2026-06-19 11:19:13 +0800

    Update fdt.py

--- drills/fdt.py
+++ drills/fdt.py
@@ -1,40 +1,46 @@
-grades = []
+def user_input():
+    grades = []
 
-while True:
-    entry = input("Input grades (or 'done' to stop): ")
-    if entry == "done":
-        break
-    try:
-        grades.append(int(entry))
-    except ValueError:
-        print("Invalid input, try again.")
-    
+    while True:
+        entry = input("Input grades (or 'done' to stop): ")
+        if entry == "done":
+            break
+        try:
+            grades.append(int(entry))
+        except ValueError:
+            print("Invalid input, try again.")
+    return grades
 
-frequency = {}
+def generate_fdt(grades):        
 
-total_students = 0
-total_grades = 0
+    frequency = {}
 
-for grade in grades:
-    if grade not in frequency:
-        frequency[grade] = 1
-    else:
-        frequency[grade] = frequency[grade] + 1
-    total_students = total_students + 1
-    total_grades = total_grades + grade
+    total_students = 0
+    total_grades = 0
 
-print("Grade | Frequency | Relative Frequency")
+    for grade in grades:
+        if grade not in frequency:
+            frequency[grade] = 1
+        else:
+            frequency[grade] = frequency[grade] + 1
+        total_students = total_students + 1
+        total_grades = total_grades + grade
 
-for grade, count in sorted(frequency.items()):
-    relative_frequency = (count/total_students) * 100
-    print(f"{grade} | {count} | {relative_frequency}%")
+    print("Grade | Frequency | Relative Frequency")
 
-avg_grades = total_grades/total_students
+    for grade, count in sorted(frequency.items()):
+        relative_frequency = (count/total_students) * 100
+        print(f"{grade} | {count} | {relative_frequency}%")
 
-most_frequent = max(frequency, key=frequency.get)
-least_frequent = min(frequency, key=frequency.get)
+    avg_grades = total_grades/total_students
 
-print(f"Total Students: {total_students}")
-print(f"Average Grades: {avg_grades}")
-print(f"Most frequent grade: {most_frequent}")
-print(f"Least frequent grade: {least_frequent}")
+    most_frequent = max(frequency, key=frequency.get)
+    least_frequent = min(frequency, key=frequency.get)
+
+    print(f"Total Students: {total_students}")
+    print(f"Average Grades: {avg_grades}")
+    print(f"Most frequent grade: {most_frequent}")
+    print(f"Least frequent grade: {least_frequent}")
+
+grades = user_input()
+generate_fdt(grades)
